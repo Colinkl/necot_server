e995cad2fb32a8ed1cab2dff2b4eb5a1afe6c2b9
El Strategico <strategico.dev@gmail.com>
2020-10-31 03:57:54 +0800

    Added method: index, show

--- app/Http/Controllers/API/v1/NewsController.php
+++ app/Http/Controllers/API/v1/NewsController.php
@@ -2,13 +2,32 @@
 
 namespace App\Http\Controllers\API\v1;
 
-use App\Http\Controllers\Controller;
+use App\Models\News;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class NewsController extends Controller
 {
-    public function index()
+    /**
+     * GET http://site.local/api/v1/news
+     *
+     * @param Request $request
+     * @return mixed
+     */
+    public function index(Request $request)
     {
-        return 'News';
+        $limit = $request->input('limit') ?? News::LIMIT;
+        return News::getLast($limit);
+    }
+
+    /**
+     * GET http://site.local/api/v1/news/{news}
+     *
+     * @param News $news
+     * @return News
+     */
+    public function show(News $news)
+    {
+        return $news;
     }
 }
