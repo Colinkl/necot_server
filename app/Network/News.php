24315fc4a22b34e59513f406a67743c63921f238
El Strategico <strategico.dev@gmail.com>
2020-10-31 01:33:27 +0800

    Added News fake model

---
+++ app/Network/News.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Network;
+
+use Illuminate\Support\Facades\Http;
+
+class News
+{
+    /**
+     * @param int $page
+     */
+    public static function get($page = 1)
+    {
+
+    }
+}
