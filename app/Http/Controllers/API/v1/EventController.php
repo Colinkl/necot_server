79673537ba29e2af9526fcec2da4fc73b1580b0c
El Strategico <strategico.dev@gmail.com>
2020-10-31 06:24:57 +0800

    Added EventController

---
+++ app/Http/Controllers/API/v1/EventController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\API\v1;
+
+use App\Models\Event;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class EventController extends Controller
+{
+    /**
+     * GET http://site.local/api/v1/events
+     *
+     * @param Request $request
+     * @return mixed
+     */
+    public function index(Request $request)
+    {
+        $limit = $request->input('limit') ?? Event::LIMIT;
+        return Event::getLast();
+    }
+
+    /**
+     * GET http://site.local/api/v1/events/{event}
+     * @param Event $event
+     * @return Event
+     */
+    public function show(Event $event)
+    {
+        return $event;
+    }
+}
