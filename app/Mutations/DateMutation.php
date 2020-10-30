07744bd3e9b87433be650982e3255660d3826ef9
El Strategico <strategico.dev@gmail.com>
2020-10-31 03:59:23 +0800

    Added DateMutation

---
+++ app/Mutations/DateMutation.php
@@ -0,0 +1,53 @@
+<?php
+
+
+namespace App\Mutations;
+
+use Carbon\Carbon;
+
+class DateMutation
+{
+    const DATES = [
+        'января'    => 'January',
+        'февраля'   => 'February',
+        'марта'     => 'March',
+        'апреля'    => 'April',
+        'мая'       => 'May',
+        'июня'      => 'June',
+        'июля'      => 'July',
+        'авугста'   => 'August',
+        'сентября'  => 'September',
+        'октября'   => 'October',
+        'ноября'    => 'November',
+        'декабря'   => 'December'
+    ];
+
+    /**
+     * Converts month names to English
+     *
+     * @param $time
+     * @return string
+     */
+    public static function toEnglish($time)
+    {
+        $date = explode(' ', $time);
+        list($day, $month, $years) = $date;
+        $month = self::DATES[$month];
+
+        return "$month $day, $years";
+    }
+
+    /**
+     * Converts to unix time
+     *
+     * @param $time
+     * @return float|int|string
+     */
+    public static function toUnix($time)
+    {
+        $time = self::toEnglish($time);
+        $date = Carbon::create($time);
+
+        return $date->timestamp;
+    }
+}
