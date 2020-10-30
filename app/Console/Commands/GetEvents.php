b65f7d9c6812efe28a87cf86bcfd50f82d69c80b
El Strategico <strategico.dev@gmail.com>
2020-10-31 06:25:12 +0800

    Added GetEvent command

---
+++ app/Console/Commands/GetEvents.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Event;
+use App\Mutations\DateMutation;
+use Illuminate\Console\Command;
+use App\Network\Events as EventNetwork;
+
+class GetEvents extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'events:get';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Get events and save to database';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->info('News saving');
+        $page = 1;
+        $saved = 0;
+
+        while($events = EventNetwork::get($page))
+        {
+            foreach($events as $event)
+            {
+                $this->line('<fg=blue>-----------------------------------</>');
+                $this->line('<fg=blue>Save record</>');
+
+                $event['date_start'] = DateMutation::toUnix($event['date_start']);
+                $event['date_end'] = DateMutation::toUnix($event['date_end']);
+
+                $this->info('Title - '. $event['title']);
+
+                Event::create($event);
+
+                $saved++;
+                $this->line('<fg=blue>-----------------------------------</>');
+            }
+            $page++;
+        }
+
+        $this->line("Saved - $saved");
+
+        return 0;
+    }
+}
