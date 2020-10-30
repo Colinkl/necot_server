09c4c7b2b084f908e54ddca8088b092432fa4e5c
El Strategico <strategico.dev@gmail.com>
2020-10-30 23:26:48 +0800

    Added NewsController

---
+++ app/Http/Controllers/API/v1/NewsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\API\v1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class NewsController extends Controller
+{
+    public function index()
+    {
+        return 'News';
+    }
+}
