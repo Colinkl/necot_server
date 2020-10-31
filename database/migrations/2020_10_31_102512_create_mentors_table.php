09e4dc8628d3f5126e35e394829fa150951979a4
El Strategico <strategico.dev@gmail.com>
2020-10-31 19:27:04 +0800

    Added migrations

---
+++ database/migrations/2020_10_31_102512_create_mentors_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMentorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mentors', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->text('description');
+            $table->string('email')->unique();
+            $table->string('avatar')->default('/uploads/mentors/default-avatar.png');
+            $table->string('telegram_link')->nullable();
+            $table->string('whatsapp_link')->nullable();
+            $table->string('vk_link')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mentors');
+    }
+}
