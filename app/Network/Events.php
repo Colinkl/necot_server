abb57b28e3631ca50bde41e24c1b5159af0b0543
Vladislav <vladislavbykovyl@gmail.com>
2020-10-31 17:44:56 +0700

    Created events parser

--- app/Network/Events.php
+++ app/Network/Events.php
@@ -12,48 +12,40 @@ class Events
      */
     public static function get($page = 1)
     {
-        if ($page == 1) $f = ''; else $f = "?page=$page";
-        $eventList = [];
-
-        $dom = new Dom;
-        $response = Http::get("https://edo.72to.ru/genius/events$f")->getBody()->getContents();
-        var_dump($response);
-        die();
-        $dom->loadStr($response);
-
-        $contents = $dom->find('.events-list-item');
+        $contents = file_get_contents("https://edo.72to.ru/genius/dict/events_list/?realization_form=");
+        $contents =  json_decode($contents, true);
 
         if(!$contents) return null;
 
-        foreach ($contents as $content) {
-            $eventItem = [];
+        $idList = [];
+        $eventList = [];
 
-            $eventItem["title"] = $content->find('.event-card__title')->find('a')->innerHtml;
-            $eventItem["link"] = "https://edo.72to.ru/".$content->find('.event-card__title')->find('a')->getAttribute('href');
-            $eventItem["avatar"] = "https://admtyumen.ru/".$content->find('.event-cart__image')->getAttribute('src');
-
-            $domEventItem = new Dom;
-            $response = Http::get($eventItem["link"])->getBody()->getContents();
-            $domEventItem->loadStr($response);
-
-            $eventItem["date_start"] = str_replace("Дата начала: ", "", $domEventItem->find('.vue-model__short-info-item')->innerHtml[0]);
-            $eventItem["date_end"] = str_replace("Дата окончания: ", "", $domEventItem->find('.vue-model__short-info-item')->innerHtml[1]);
-            $eventItem["location"] = str_replace("Место проведения: ", "", $domEventItem->find('.vue-model__short-info-item')->innerHtml[2]);
-            $domEventItem->find('.vue-model-description__aside-items');
-            $eventItem["focuses"] = is_array($domEventItem[0]->find('.vue-model-description__aside-list-item')) ? $domEventItem[0]->find('.vue-model-description__aside-list-item') : [$domEventItem[0]->find('.vue-model-description__aside-list-item')];
-            $eventItem["profiles"] = is_array($domEventItem[1]->find('.vue-model-description__aside-list-item')) ? $domEventItem[1]->find('.vue-model-description__aside-list-item') : [$domEventItem[1]->find('.vue-model-description__aside-list-item')];
-            $eventItem["form_of_conducting"] = $domEventItem[2]->find('.vue-model-description__aside-list-item');
-            $eventItem["event_type"] = $domEventItem[3]->find('.vue-model-description__aside-list-item');
-            $eventItem["event_level"] = $domEventItem[4]->find('.vue-model-description__aside-list-item');
-            $eventItem["participant_category"] = $domEventItem[5]->find('.vue-model-description__aside-list-item');
-            $eventItem["organizer"] = $domEventItem[6]->find('.vue-model-description__aside-list-item');
-            $eventItem["curator"] = $domEventItem[7]->find('.vue-model-description__aside-list-item');
-
-            $eventList[] = $eventItem;
+        foreach ($contents["data"] as $content) {
+            $idList[] = $content["id"];
         }
 
-        return $eventList;
+        foreach ($idList as $eventID) {
+            $eventItem = json_decode(file_get_contents("https://edo.72to.ru/genius/events/$eventID/data"), true)['data'];
+            $eventListItem = [];
 
+            $eventListItem["title"] = $eventItem['name'];
+            $eventListItem["link"] = "https://edo.72to.ru/genius/events/$eventID";
+            $eventListItem["avatar"] = $eventItem['cover'];
+            $eventListItem["date_start"] = $eventItem['date_begin'];
+            $eventListItem["date_end"] = $eventItem['date_end'];
+            $eventListItem["location"] = $eventItem['place'];
+            $eventListItem["focuses"] = $eventItem['direction_gifted'];
+            $eventListItem["profiles"] = $eventItem['kind_direction_gifted'];
+            $eventListItem["form_of_conducting"] = $eventItem['realization_form'];
+            $eventListItem["event_type"] = $eventItem['kind'];
+            $eventListItem["event_level"] = $eventItem['level'];
+            $eventListItem["participant_category"] = $eventItem['participants_category'];
+            $eventListItem["organizer"] = $eventItem['unit_organizer'];
+            $eventListItem["curator"] = $eventItem['curator'];
+
+            $eventList[] = $eventListItem;
+        }
+        return $eventList;
     }
 }
 
